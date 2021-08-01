c107ba4e1ab811dcc6cd0302a51c169b41a28f86
Halyna Urbanska <webenot@mail.ua>
2021-08-01 11:15:44 +0300

    feat: Add test events graphql schemas and resolvers

--- app.js
+++ app.js
@@ -5,16 +5,55 @@ const { buildSchema } = require('graphql');
 
 require('dotenv').config();
 
+const EVENTS = [
+  {
+    _id: '1',
+    title: 'Romantic Cooking',
+    description: 'Romantic Cooking description',
+    price: 10,
+    date: '01-08-2021',
+  },
+  {
+    _id: '2',
+    title: 'Sailing',
+    description: 'Sailing description',
+    price: 20,
+    date: '02-08-2021',
+  },
+  {
+    _id: '3',
+    title: 'All Night Coding',
+    description: 'All Night Coding description',
+    price: 30,
+    date: '03-08-2021',
+  },
+];
+
 const app = express();
 app.use(bodyParser.json());
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+    
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+  
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
     
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
     
     schema {
@@ -23,8 +62,25 @@ app.use('/graphql', graphqlHTTP({
     }
   `),
   rootValue: {
-    events: () => [ 'Romantic Cooking', 'Sailing', 'All Night Coding' ],
-    createEvent: ({ name }) => name,
+    events: () => EVENTS,
+    createEvent: ({
+      eventInput: {
+        title,
+        description,
+        price,
+        date,
+      },
+    }) => {
+      const event = {
+        _id: Math.random().toString(),
+        title,
+        description,
+        price,
+        date,
+      };
+      EVENTS.push(event);
+      return event;
+    },
   },
   graphiql: process.env.NODE_ENV === 'development',
 }));
