9c3f0b95a02b9387488ea78b8f13bbddb85d4ee1
Halyna Urbanska <webenot@mail.ua>
2021-08-01 10:30:59 +0300

    feat: Add test graphql schemas and resolvers

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+    
+    type RootMutation {
+      createEvent(name: String): String
+    }
+    
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => [ 'Romantic Cooking', 'Sailing', 'All Night Coding' ],
+    createEvent: ({ name }) => name,
+  },
+  graphiql: process.env.NODE_ENV === 'development',
+}));
 
 const PORT = Number(process.env.PORT) || 5000;
 
