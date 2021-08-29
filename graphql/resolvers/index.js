0e72b26b6fd30a25337030229dacdebc3eea159a
Halyna Urbanska <webenot@mail.ua>
2021-08-29 18:46:24 +0300

    chore: Refactoring resolvers

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,9 @@
-const { events, createEvent } = require('graphql/resolvers/event.resolver');
-const { bookings, bookEvent, cancelBooking } = require('graphql/resolvers/booking.resolver');
-const { createUser } = require('graphql/resolvers/user.resolver');
+const eventResolvers = require('graphql/resolvers/event.resolver');
+const bookingResolvers = require('graphql/resolvers/booking.resolver');
+const userResolvers = require('graphql/resolvers/user.resolver');
 
 module.exports = {
-  events,
-  bookings,
-  createEvent,
-  createUser,
-  bookEvent,
-  cancelBooking,
+  ...eventResolvers,
+  ...bookingResolvers,
+  ...userResolvers,
 };
